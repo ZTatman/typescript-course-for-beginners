cbfb4de7bc5a1d88a4c0d013ebe3bfb9e92aeaff
ZTatman <zachariah.tatman@charter.com>
2023-07-10 14:40:25 -0400

    update: the difference between JS types and TS types added.

--- 01/app.ts
+++ 01/app.ts
@@ -38,3 +38,22 @@ function addTyped(num1: number, num2: number) {
  *       This only helps during development as it points out mistakes*/
 console.log(":: result3: ", addTyped(number3, number2))
 
+
+/* ------ JavaScript Types vs. Typescript Types ------ */
+// The core types mentioned above arent exlusive to just typescript...
+// Javascript already knows about some types like number, boolean, and string.
+// So then what is the difference between these types in JS vs TS?
+
+
+// Well, the difference is that JavaScript uses "dynamic types". The type of a 
+// variable in JavaScript is not known/checked until runtime! This means that if there
+// is a type error you won't know about it until executing the code including the type error.
+
+// Typescript uses "static types" set during development.
+// For example a runtime check for numbers only using JS would look like the following.
+function sum(num1, num2) {
+    if (typeof num1 !== "number" || typeof num2 !== "number") {
+        throw new Error("function: sum can only accept parameters of type: number!")
+    }
+    return num1 + num2;
+}
